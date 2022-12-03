b94a308ecd9beaf314920ed6cc96579f9d54d72d
churchill <churrgift@gmail.com>
2022-12-03 14:16:33 -0800

    new update

---
+++ 0x02-python-import_modules/eg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import egg
+print("{}".format(__name__))
---
+++ 0x02-python-import_modules/egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    print("in {}".format(__name__))
+else:
+    print("{} not in".format(__name__))
