7fec71116b11fc53ad4a40c52339fac471ccb254
churchill <churrgift@gmail.com>
2022-12-05 13:23:34 -0800

    new update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     newstring= ""
     for i in range(len(my_string)):
-        if my_string[i] == 'c':
+        if my_string[i] == 'c' or my_string[i] =='C':
             continue
         newstring+=my_string[i]
     return newstring
