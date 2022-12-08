7214def93b803227d186e75c8d794a0158ec9978
churchill <churrgift@gmail.com>
2022-12-07 20:56:33 -0800

    new update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None)
+    if (not isinstance(roman_string, str) or roman_string == None)
         return (0)
     roman_dict = {
             "I": 1,
@@ -16,9 +15,8 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1   
+        if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1   
         else:
             num += roman_dict[roman_string[i]]
     return (num)
